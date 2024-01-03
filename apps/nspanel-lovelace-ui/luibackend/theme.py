70a3bec261f0b9303859f37a4c58e0b9d121e508
isntfunny <78227791+isntfunny@users.noreply.github.com>
2024-01-03 19:13:32 +0100

    Update theme.py

--- apps/nspanel-lovelace-ui/luibackend/theme.py
+++ apps/nspanel-lovelace-ui/luibackend/theme.py
@@ -1,6 +1,8 @@
 
 from helper import rgb_dec565
 
+import apis
+
 default_screensaver_color_mapping = {
     #"item":            "color in decimal RGB565 (0-65535)"
     "background":       "0",
@@ -29,7 +31,14 @@ def get_screensaver_color_output(theme, state=None):
 
 def map_color(key, theme):
     config_color = default_screensaver_color_mapping[key]
-#   Use theme color if set
+    # Use theme color if set and is a string
     if key in theme:
-        config_color = rgb_dec565(theme[key])
+        theme_value = theme[key]
+        if isinstance(theme_value, str):
+            # Get the state and split it into an array of three numbers
+            state_value = apis.ha_api.get_state(theme_value)
+            apis.ha_api.log(state_value)
+            theme_value = [int(x) for x in state_value.split(',')]
+        apis.ha_api.log(theme_value)
+        config_color = rgb_dec565(theme_value)
     return config_color
